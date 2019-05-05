612368b787fdb5ede15811a3ac360e2f8672eff3
owengiri20 <46738862+owengiri20@users.noreply.github.com>
2019-05-05 15:25:52 +0800

    ...

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 // basic closure example
-var passed = 3;
+// var passed = 3;
 
-var addTo = function() {
-  var inner = 2;
-  return passed + inner;
+// var addTo = function() {
+//   var inner = 2;
+//   return passed + inner;
+// };
+
+// console.dir(addTo);
+
+var addTo = function(passed) {
+  var add = function(inner) {
+    return passed + inner;
+  };
+  return add;
 };
+// console.dir(addTo(3));
+var addThree = new addTo(3);
+var addFour = new addTo(4);
 
-console.log(addTo());
+// console.dir(addThree);
+// console.dir(addFour);
+
+console.log(addThree(1));
+console.log(addFour(1));
